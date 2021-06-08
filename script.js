ef9ec45847e5a27b6295d06c04fedcefd550a5c1
Viktoria Toth <k.viki1117@gmail.com>
2021-06-08 17:35:23 +0200

    header tag changed added js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function loadEvent() {
+    let menuContainer = `<div id="menuContainer"></div>`;
+    let menuBtn = document.getElementById("menuBtn");
 
+    menuBtn.insertAdjacentHTML("beforeend", menuContainer);
+
+    const menuList = [
+        {"name": "Menu1", "link": "url"},
+        {"name": "Menu2", "link": "url"},
+        {"name": "Menu3", "link": "url"},
+        {"name": "Menu4", "link": "url"}
+    ];
+
+    for (let listItem of menuList){
+        let link = `
+            <a href="${listItem.link}">${listItem.name}</a>
+        `;
+        document.getElementById("menuContainer").insertAdjacentHTML("beforeend", link);        
+    }
 };
 
-window.addEventListener("load", _load);
+window.addEventListener("load", loadEvent);
+
+/* window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth'
+  }); */
